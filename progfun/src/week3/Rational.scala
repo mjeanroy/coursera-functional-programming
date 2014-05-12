b2bcb38eb1c3c20a00d6db784890b30a9a2a8789
mjeanroy <mickael.jeanroy@gmail.com>
2014-05-12 17:28:24 +0200

    add week3 samples

---
+++ progfun/src/week3/Rational.scala
@@ -0,0 +1,27 @@
+package week3
+
+class Rational(x: Int, y: Int) {
+  require(y != 0, "denominator must be nonzero")
+
+  def this(x: Int) = this(x, 1)
+
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+  val g = gcd(x, y)
+  def numer = x / g
+  def denom = y / g
+
+  def +(that: Rational) =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom)
+
+  def <(that: Rational) = numer * that.denom < that.numer * denom
+
+  def max(that: Rational) = if (this < that) that else this
+
+  def unary_- : Rational = new Rational(-numer, denom)
+
+  def -(that: Rational): Rational = this + -that
+
+  override def toString = numer + "/" + denom
+}
